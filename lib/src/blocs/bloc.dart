0061f52dc7dd32f95b454592d5f7a5bc6adf2503
Ely Alamillo <ely0013@gmail.com>
2019-06-24 21:53:44 -0500

    add validators mixin and applied it to getters

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,14 +1,23 @@
 import 'dart:async';
 
-class Bloc {
+import 'validators.dart';
+
+class Bloc with Validators {
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
-  // add data
+  // add data to stream
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
-  // get data
-  Stream<String> get email => _emailController.stream;
-  Stream<String> get password => _passwordController.stream;
+  // get data from stream
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password =>
+      _passwordController.stream.transform(validatePassword);
+
+  // closes streams when we want
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
 }
